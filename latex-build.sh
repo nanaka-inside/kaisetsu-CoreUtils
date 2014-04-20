178891ce4d9c92c6011dd0b1e6da3bff48180503
mtgto <hogerappa@gmail.com>
2014-04-20 10:56:32 +0900

    Separate abstract and body section.

--- latex-build.sh
+++ latex-build.sh
@@ -7,5 +7,7 @@ TEXFILE=_build/latex/coreutils.tex
 #sed -i -e 's/commandchars/frame=single,commandchars/' $TEXFILE
 
 cd _build/latex/
+# 前書きの前に入るtableofcontentsを削除
+sed -i -e '120,140s/\\tableofcontents//' coreutils.tex
 platex coreutils.tex && platex coreutils.tex && dvipdfmx -p b5 -r 600 -v -V 5 -o index-b5.pdf coreutils.dvi
 # platex Nanaka-inside-c83.tex && dvipdfmx -p b5 -c -r 600 -v -V 3 -o index-b5.pdf Nanaka-inside-c83.dvi
